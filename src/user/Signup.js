a04dfbff6c7dd2938b2246ff6d1aaeb3ff788582
Barron Rodriguez <bigbbiscuit@gmail.com>
2020-03-12 18:24:33 -0700

    Darrin test

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -23,7 +23,19 @@ class Signup extends Component {
             email,
             password
         };
-       // console.log(user);
+        console.log(user); 
+/*        fetch("http://localhost:8080/signup", {
+           method: "POST",
+           headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"   
+           },
+           body: JSON.stringify(user)
+       })
+       .then(response => {
+           return response.json()
+       })
+       .catch(err => console.log(err)) */
     };
 
     render() {
