848207aa2311a06f4f465eb31a6353e72cb92b29
Gabriel Castro <43317756+castrogabe@users.noreply.github.com>
2020-04-08 17:13:40 -0700

    Add files via upload

---
+++ core/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Posts from "../post/Posts";
+
+const Home = () => (
+  <div>
+    <div className='container'>
+      <div className='jumbotron'>
+        <h2>DevLife</h2>
+        <p className='lead'>A healthy social network for developers</p>
+      </div>
+    </div>
+    <div className="container">
+      <Posts />
+    </div>
+  </div>
+);
+
+export default Home;
